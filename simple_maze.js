475dc281dec2a9b40cc1c1d6b851d4d3fc57515c
sinanm89 <sinanm89@gmail.com>
2017-07-06 14:58:45 -0400

    size decreased to 8

--- simple_maze.js
+++ simple_maze.js
@@ -8,7 +8,7 @@ var pixel_height = w * cell_size*100;
 
 var directions = [-w, +1, +w, -1];
 
-var cell_size = 10;
+var cell_size = 8;
 
 var canvas = d3.select("body").append("canvas")
   .attr("width", w*cell_size +10)
